a1e0b1e2590071fc12a6c12f601b200799f2e889
李昌南 <changnan666@gmail.com>
2022-07-17 23:07:23 +0800

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,64 @@
 import { TransitionEventHandler, useEffect, useRef, useState } from "react";
 import "./App.css";
 
+const wait = () => new Promise((resolve) => setTimeout(resolve, 200));
+
 const App = () => {
   const containerRef = useRef<HTMLDivElement | null>(null);
-  const words = useRef(["a", "b"]);
+  const words = useRef(["hello", "world"]);
   const [index, setIndex] = useState(-1);
+  const lastIndex = useRef(-1);
 
   const onTransitionEnd: TransitionEventHandler<HTMLDivElement> = (e) => {
-    console.log(e.target);
+    const target = e.target as HTMLElement;
+    const container = target.parentElement as HTMLSpanElement;
+    if (container.classList.contains("hide")) {
+      container.classList.remove("hide");
+    }
+  };
+
+  const action = async (item: HTMLDivElement) => {
+    if (!item || !item.children) return;
+
+    let i = 0;
+    for await (const span of Array.from(item.children)) {
+      await wait();
+      span.classList.add("show");
+
+      const lastChildren =
+        item.parentElement && item.parentElement.children[lastIndex.current];
+      if (lastChildren) {
+        const nextSpan = lastChildren.children[i];
+
+        if (nextSpan && nextSpan.classList.contains("show")) {
+          nextSpan.classList.remove("show");
+          nextSpan.classList.add("hide");
+        }
+      }
+
+      i++;
+    }
   };
 
   useEffect(() => {
     setTimeout(() => {
-      setIndex(0);
-    }, 100);
-  }, []);
+      let i = index;
+      i++;
+      i %= words.current.length;
+      setIndex(i);
+    }, 2000);
+  }, [index]);
+
+  useEffect(() => {
+    if (index < 0) return;
+    const target = containerRef.current;
+    if (!target) return;
+
+    console.log(index);
+
+    const item = target.children[index] as HTMLDivElement;
+    action(item);
+  }, [index]);
 
   return (
     <div className="container" ref={containerRef}>
@@ -22,7 +66,11 @@ const App = () => {
         return (
           <div key={i} className="item" onTransitionEnd={onTransitionEnd}>
             {item.split("").map((item, j) => {
-              return <span key={i + j}>{item}</span>;
+              return (
+                <span key={i + j}>
+                  <i>{item}</i>
+                </span>
+              );
             })}
           </div>
         );
