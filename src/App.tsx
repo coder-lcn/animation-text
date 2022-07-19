dfd07e3075db2ceb881c07eafbcf471fcca19666
123falksdfj <changnan666@gmail.com>
2022-07-19 19:09:35 +0800

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,51 @@
-import { TransitionEventHandler, useEffect, useRef, useState } from "react";
+import { TransitionEventHandler, useCallback, useEffect, useRef, useState } from "react";
 import "./App.css";
 
-const wait = () => new Promise((resolve) => setTimeout(resolve, 100));
+// 开始切换下一个单词的间隔时间
+const word_wait = 200;
+
+// 切换到下一个字母的间隔时间
+const letter_wait = 100;
+
+const wait = (t: number) => new Promise(resolve => setTimeout(resolve, t))
 
 const App = () => {
   const containerRef = useRef<HTMLDivElement | null>(null);
-  const words = useRef(["hello", "world", "hahah"]);
+  const words = useRef(["hellooooo", "world", "hahah"]);
   const [index, setIndex] = useState(0);
   const lastIndex = useRef(-1);
+  const indexRef = useRef(0);
 
-  const onTransitionEnd: TransitionEventHandler<HTMLDivElement> = (e) => {
+  const onTransitionEnd: TransitionEventHandler<HTMLDivElement> = useCallback(async (e) => {
     const target = e.target as HTMLElement;
     const container = target.parentElement as HTMLSpanElement;
+
     if (container.classList.contains("hide")) {
       container.classList.remove("hide");
     }
-  };
+
+    if (container.classList.contains('show')) {
+      if (container.parentElement && container === container.parentElement.lastElementChild) {
+        let i = indexRef.current + 1;
+        i %= words.current.length;
+        await wait(word_wait)
+        setIndex(i)
+      }
+    }
+  }, []);
 
   const action = async (item: HTMLDivElement) => {
     if (!item || !item.children) return;
 
     let i = 0;
+    const lastChildren =
+      item.parentElement && item.parentElement.children[lastIndex.current];
+
     for await (const span of Array.from(item.children)) {
-      await wait();
+      await wait(letter_wait);
       span.classList.add("show");
 
-      const lastChildren =
-        item.parentElement && item.parentElement.children[lastIndex.current];
+
       if (lastChildren) {
         const nextSpan = lastChildren.children[i];
 
@@ -38,24 +57,37 @@ const App = () => {
 
       i++;
     }
+
+    if (lastChildren) {
+      const loss = Array.from(lastChildren.children).filter(item => item.classList.contains('show'))
+      for (const nextSpan of loss) {
+        nextSpan.classList.remove("show");
+        nextSpan.classList.add("hide");
+      }
+    }
   };
 
-  useEffect(() => {
-    lastIndex.current = index - 1;
-    if (lastIndex.current < 0) lastIndex.current = words.current.length - 1;
+  // useEffect(() => {
+  //   lastIndex.current = index - 1;
+  //   if (lastIndex.current < 0) lastIndex.current = words.current.length - 1;
 
-    setTimeout(() => {
-      let i = index;
-      i++;
-      i %= words.current.length;
-      setIndex(i);
-    }, 2000);
-  }, [index]);
+  //   setTimeout(() => {
+  //     let i = index;
+  //     i++;
+  //     i %= words.current.length;
+  //     setIndex(i);
+  //   }, 2000);
+  // }, [index]);
 
   useEffect(() => {
+    indexRef.current = index;
+
     if (index < 0) return;
     const target = containerRef.current;
     if (!target) return;
+
+    lastIndex.current = index - 1;
+    if (lastIndex.current < 0) lastIndex.current = words.current.length - 1;
 
     const item = target.children[index] as HTMLDivElement;
     action(item);
